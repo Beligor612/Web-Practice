61252fa5e00b880f11ae58c8b4f5b6986acdcbd8
Igor Belov <48245759+Beligor612@users.noreply.github.com>
2020-08-12 15:55:13 +0300

    Project TodoList

---
+++ TodoList/script.js
@@ -0,0 +1,315 @@
+const tasks = [
+    {
+      _id: '5d2ca9e2e03d40b326596aa7',
+      completed: false,
+      body:
+        'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+      title: 'Eu ea incididunt sunt consectetur fugiat non.',
+    },
+    {
+      _id: '5d2ca9e29c8a94095c1288e0',
+      completed: false,
+      body:
+        'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+      title:
+        'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+    },
+    {
+      _id: '5d2ca9e2e03d40b3232496aa7',
+      completed: false,
+      body:
+        'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+      title: 'Eu ea incididunt sunt consectetur fugiat non.',
+    },
+    {
+      _id: '5d2ca9e29c8a94095564788e0',
+      completed: false,
+      body:
+        'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+      title:
+        'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+    },
+  ];
+  
+  (function(arrOfTasks) { // Function in function
+      const objOfTasks = arrOfTasks.reduce((acc, task) => {
+        acc[task._id] = task;
+        return acc;
+      },{}) // Reduce array in object
+      
+    //   Themes
+      const themes = {
+        default: {
+          '--base-text-color': '#212529',
+          '--header-bg': '#007bff',
+          '--header-text-color': '#fff',
+          '--default-btn-bg': '#007bff',
+          '--default-btn-text-color': '#fff',
+          '--default-btn-hover-bg': '#0069d9',
+          '--default-btn-border-color': '#0069d9',
+          '--danger-btn-bg': '#dc3545',
+          '--danger-btn-text-color': '#fff',
+          '--danger-btn-hover-bg': '#bd2130',
+          '--danger-btn-border-color': '#dc3545',
+          '--input-border-color': '#ced4da',
+          '--input-bg-color': '#fff',
+          '--input-text-color': '#495057',
+          '--input-focus-bg-color': '#fff',
+          '--input-focus-text-color': '#495057',
+          '--input-focus-border-color': '#80bdff',
+          '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
+        },
+        dark: {
+          '--base-text-color': '#212529',
+          '--header-bg': '#343a40',
+          '--header-text-color': '#fff',
+          '--default-btn-bg': '#58616b',
+          '--default-btn-text-color': '#fff',
+          '--default-btn-hover-bg': '#292d31',
+          '--default-btn-border-color': '#343a40',
+          '--default-btn-focus-box-shadow':
+            '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+          '--danger-btn-bg': '#b52d3a',
+          '--danger-btn-text-color': '#fff',
+          '--danger-btn-hover-bg': '#88222c',
+          '--danger-btn-border-color': '#88222c',
+          '--input-border-color': '#ced4da',
+          '--input-bg-color': '#fff',
+          '--input-text-color': '#495057',
+          '--input-focus-bg-color': '#fff',
+          '--input-focus-text-color': '#495057',
+          '--input-focus-border-color': '#78818a',
+          '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+        },
+        light: {
+          '--base-text-color': '#212529',
+          '--header-bg': '#fff',
+          '--header-text-color': '#212529',
+          '--default-btn-bg': '#fff',
+          '--default-btn-text-color': '#212529',
+          '--default-btn-hover-bg': '#e8e7e7',
+          '--default-btn-border-color': '#343a40',
+          '--default-btn-focus-box-shadow':
+            '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+          '--danger-btn-bg': '#f1b5bb',
+          '--danger-btn-text-color': '#212529',
+          '--danger-btn-hover-bg': '#ef808a',
+          '--danger-btn-border-color': '#e2818a',
+          '--input-border-color': '#ced4da',
+          '--input-bg-color': '#fff',
+          '--input-text-color': '#495057',
+          '--input-focus-bg-color': '#fff',
+          '--input-focus-text-color': '#495057',
+          '--input-focus-border-color': '#78818a',
+          '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+        },
+      };
+    let lastSelectedTheme = localStorage.getItem('app_theme') || 'default';
+    setTheme(lastSelectedTheme)
+    //   Elements UI
+    const listContainer = document.querySelector('.tasks-list-section .list-group')
+    const form = document.forms['addTask']
+    const inputTitle = form.elements['title'];
+    const inputBody = form.elements['body']
+    const spanDanger = document.querySelector('.text-danger')
+    const taskButtons = document.querySelector('.task-buttons')
+    
+    const select = document.getElementById('themeSelect')
+    //   Events
+    renderTasks(objOfTasks)
+    form.addEventListener('submit', onFormSubmitHandler) //Event for form
+
+    taskButtons.addEventListener('click', onClickTaskListButtons)//Event for button task list
+
+    listContainer.addEventListener('click', onDeleteFinishHandler)// Event for Delete-Finish buttons in task
+
+    select.addEventListener('change', onThemeSelectHandler)
+      function renderTasks(taskList){ // Rendering fragment in HTML
+          if (!taskList){
+              console.error("Таск лист не был передан");
+              return;
+          }
+
+          const fragment = document.createDocumentFragment();
+          Object.values(taskList).forEach(task => {
+              const li = listItemTemplate(task);
+              fragment.appendChild(li)
+          })
+         
+          listContainer.appendChild(fragment)
+  
+      }
+      function listItemTemplate({ _id, title, body } = {}){ // Create new task for fragment
+        const li = document.createElement('li');
+        li.classList.add('list-group-item', 
+                        'd-flex', 
+                        'align-items-center', 
+                        'flex-wrap', 
+                        'mt-2')
+        li.setAttribute('data-task-id', _id)
+        const span = document.createElement('span')
+        span.textContent = title;
+        span.style.fontWeight = 'bold';
+
+        const btn = document.createElement('button');
+        btn.textContent = "Delete";
+        btn.classList.add('btn',
+                        'btn-danger',
+                        'delete-btn')
+        
+        const btnSuccess = document.createElement('button');
+        btnSuccess.textContent = 'Finish';
+        btnSuccess.classList.add('btn',
+                        'btn-success',
+                        'ml-auto',
+                        'success-btn')
+
+        const p = document.createElement('p');
+        p.classList.add('mt-2', 'w-100')
+        p.textContent= body;
+
+        li.appendChild(span);
+        li.appendChild(p);
+        li.appendChild(btn);
+        li.appendChild(btnSuccess)
+        
+
+        return li;
+      }
+      function onFormSubmitHandler(e){ // Create new task from form
+        e.preventDefault();
+        const titleValue = inputTitle.value;
+        const bodyValue = inputBody.value;
+
+        if(!titleValue || !bodyValue){
+            alert('Пожалуйста введите title и body')
+            return;
+        }
+        const task = createNewTask(titleValue,bodyValue)
+        const listItem = listItemTemplate(task)
+        listContainer.insertAdjacentElement('afterbegin', listItem)
+        form.reset()
+      }
+      function createNewTask(title, body){ // Create new task in array
+        const newTask = {
+            title,
+            body,
+            complated: false,
+            _id: `task-${Math.random()}`
+        }
+        objOfTasks[newTask._id] = newTask;
+        isFullTaskList();
+        return { ...newTask }
+      }
+      function deleteTask(id){ // Delete task from array
+          const isConfirm = confirm('Вы хотите удалить задачу ?')
+          if (!isConfirm) return isConfirm;
+          delete objOfTasks[id];
+          return isConfirm;
+      }
+      function deleteTaskFromHTML(element, confirm){ // Delete task from HMTL
+          if (confirm){
+            element.remove();
+          }
+      }
+      function isFullTaskList(){ // Checked full task list
+        const lenOfObject = Object.keys(objOfTasks).length
+        if(!lenOfObject){
+            spanDanger.classList.remove('d-none')
+        } else spanDanger.classList.add('d-none')
+      }
+      function onDeleteFinishHandler({ target }){ // Check targeting in document
+          if(target.classList.contains('delete-btn')){ // Button delete task
+              const parent = target.closest('[data-task-id]')
+              const id = parent.dataset.taskId;
+              const confirm = deleteTask(id);
+              deleteTaskFromHTML(parent, confirm)
+              isFullTaskList()
+          } 
+          if(target.classList.contains('success-btn')){ // Button finish task
+              const parent = target.closest('.list-group-item');
+              const parentId = parent.dataset.taskId;
+              parent.classList.add('bg-success')
+                console.log(objOfTasks[parentId].completed = true);
+                console.log(listContainer);
+                if (listContainer.classList.contains('unfinished')){
+                    hideTask(parent)
+                }
+                if (listContainer.classList.contains('all-task')){
+                    grabTask(parent)
+                }
+          }
+      }
+      function onClickTaskListButtons({ target }){ //Check targeting in task-buttons
+        console.log('Unfinished-task');
+        // console.log(target);
+        if (target.classList.contains('unfinished-tasks')){
+            listContainer.classList.remove('all-task')
+            listContainer.classList.add('unfinished')
+            console.log(listContainer);
+
+            Object.keys(objOfTasks).forEach(value => {
+                const completed = objOfTasks[value].completed;
+                let id = objOfTasks[value]._id
+                let element = listContainer.querySelector(`[data-task-id = "${id}"]`)
+                if (completed){
+                    hideTask(element)
+                } else {
+                    unhideTask(element)
+                }
+            })
+        }
+          if (target.classList.contains('all-tasks')){
+            listContainer.classList.remove('unfinished')
+            listContainer.classList.add('all-task')
+            console.log(listContainer);
+
+            Object.keys(objOfTasks).forEach(value => {
+                let id = objOfTasks[value]._id
+                let element = listContainer.querySelector(`[data-task-id = "${id}"]`)
+                insertTask(value)
+                unhideTask(element)
+            });
+        }
+      }
+      function insertTask(value){
+          console.log('INSERTING...');
+        const completed = objOfTasks[value].completed;
+        let id = objOfTasks[value]._id
+        let element = listContainer.querySelector(`[data-task-id = "${id}"]`)
+        if (completed){
+            grabTask(element)
+        }
+      }
+      function grabTask(task){
+          listContainer.append(task)
+      }
+      function hideTask(task){
+        task.classList.remove('d-flex')
+        task.classList.add('d-none')
+      }
+      function unhideTask(task){
+        task.classList.remove('d-none')
+        task.classList.add('d-flex')
+      }
+
+      function onThemeSelectHandler(e){
+        const selectedTheme = select.value;
+        const isConfirmed = confirm('Вы действительно хотите изменить тему?')
+        if (!isConfirmed){
+            select.value = lastSelectedTheme;
+            return;
+        } 
+        localStorage.setItem('app_theme', selectedTheme)
+        setTheme(selectedTheme);
+        lastSelectedTheme = select.value
+      }
+      function setTheme(name){
+        console.log(name);
+        const selectedThemeObj = themes[name];
+        Object.entries(selectedThemeObj).forEach(([key,value]) =>{
+            document.documentElement.style.setProperty(key, value)
+        })
+      }
+  })(tasks);
+  
